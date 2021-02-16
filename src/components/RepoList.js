b3676d3090990a6a4008913d8f10ba0e76a91775
zander007 <zanderthuo@gmail.com>
2021-02-16 10:10:27 +0300

    fixed Favorites feature

--- src/components/RepoList.js
+++ src/components/RepoList.js
@@ -1,71 +1,94 @@
-import React from 'react';
-import { Container, Card, Col, Button, Row} from 'react-bootstrap'
-import { Redirect, Link } from 'react-router-dom';
-
+import React, {useState, useEffect} from 'react'
+import {IoIosHeart,IoIosHeartEmpty} from 'react-icons/io'
+import {  Card, Button, Row} from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import axios from 'axios'
 
 import '../styles/Repositories.css'
 
 
-// window.localStorage.setItem("repos", JSON.stringify(repos));
+const CharacterItem = () => {
+  const [repos, setRepos] = useState([]);
+  const [favorites, setFavorites] = useState([]);
+  const getArray = JSON.parse(localStorage.getItem('favorites') || '0');
+
+  useEffect(() => {
+   const result = axios.get(`https://api.github.com/repositories`)
+   .then(response => setRepos(response.data));
+   console.log(repos);
+
+}, [])
 
 
-class RepoList extends React.Component {
-  constructor(props) {
-    super(props)
+  useEffect(() => {
+    if (getArray !== 0){
+      setFavorites([...getArray])
+    }
+  }, [])
 
-    this.state = {
-      repos:[],
-      isLoading: false,
+  const addFav = (props: any) => {
+    let array = favorites;
+    let addArray = true;
+    array.map((item: any, key: number) => {
+      if (item === props.i){
+        array.splice(key, 1);
+        addArray = false;
+      }
+    });
+    if (addArray){
+      array.push(props.i);
+    }
+    setFavorites([...array])
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+
+    var storage = localStorage.getItem('favItem' + (props.i) || '0')
+    if (storage == null) {
+      localStorage.setItem(('favItem' + (props.i)), JSON.stringify(props.items));
+    }else {
+      localStorage.removeItem('favItem' + (props.i));
     }
   }
 
 
-  componentDidMount = () => {
-      axios.get(`https://api.github.com/repositories`)
-        .then(res => {
-          const repos = res.data;
-          console.log(repos)
-          this.setState({ repos });
-        })
-        .catch(error => {
-          console.log(error);
-        });
-  }
+  var items;
+  var i;
 
-
-  render(){
-    const { redirect } = this.state;
-
-     if (redirect) {
-       return <Redirect to='/singleRepo'/>;
-     }
-    return (
-        <div className="container">
-            <div className="Row">
-                {this.state.repos.map((repos) => (
-                    <Card className="RepoCard" style={{ width: '18rem' }}>
-                        <>
-                          <Card.Header className="cardHeader" as="h2">
-                              <Card.Title key={repos.id}>{repos.name}</Card.Title>
-                          </Card.Header>
-                          <Card.Body className="cardBody">
-                              <Card.Text className="cardText">
-                                  {repos.description}
-                              </Card.Text>
-                          </Card.Body>
-                          <Card.Footer className="cardFooter">
-                            <Button>
-                              <Link to={`/singleRepo/${repos.id}`}>View Repo</Link>
-                            </Button>
-                          </Card.Footer>
-                        </>
-                  </Card>
-                  ))}
-            </div>
+  return (
+    <div className="container">
+        <div className="Row">
+            {repos.map((repos) => (
+                <Card className="RepoCard"  style={{ width: '18rem' }}>
+                  <Card.Header className="cardHeader" as="h2">
+                      <div>
+                        {favorites.includes(i) ? (
+                          <IoIosHeart
+                              onClick={() => addFav({ items, i })}
+                              style={{color: 'red' }}
+                          />
+                      ) : (
+                          <IoIosHeartEmpty
+                              onClick={() => addFav({ items, i })}
+                              style={{ color: 'red' }}
+                          />
+                      )}
+                      </div>
+                      <Card.Title key={repos.id}>{repos.name}</Card.Title>
+                  </Card.Header>
+                  <Card.Body className="cardBody">
+                      <Card.Text className="cardText">
+                          {repos.description}
+                      </Card.Text>
+                  </Card.Body>
+                  <Card.Footer className="cardFooter">
+                    <Button>
+                      <Link to={`/singleRepo/${repos.id}`}>View Repo</Link>
+                    </Button>
+                  </Card.Footer>
+              </Card>
+              ))}
         </div>
-    )
-  }
+    </div>
+  )
 }
 
-export default RepoList
+export default CharacterItem
